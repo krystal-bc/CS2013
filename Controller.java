bcc3f57e088971a36040617a7ab031b231c5586c
Krystal Bernal <krystalbc@gmail.com>
2018-10-30 23:52:43 -0700

    updated display and controller javafx
    Migrated print outs to javafx display

--- Controller.java
+++ Controller.java
@@ -2,35 +2,47 @@ package intelliGreen;
 
 import java.util.ArrayList;
 
-public class Controller {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Controller extends Application {
+
+	static ArrayList<Integer> current = new ArrayList<>();
+	ArrayList<Integer> desired = new ArrayList<>();
+
+	static SensorModule simulator = new SensorModule();
+	HardwareModule hardware = new HardwareModule();
+
+	Display display = new Display();
 
 	public static void main(String[] args) {
+		current.add(0, simulator.generateTemperature());
+		current.add(1, simulator.generateHumidity());
+		current.add(2, simulator.generateMoisture());
+		current.add(3, simulator.generatePH());
+		current.add(4, simulator.generateCO2());
+		Application.launch(args);
+	}
 
-		ArrayList<Integer> current = new ArrayList<>();
-		ArrayList<Integer> desired = new ArrayList<>();
-
-		SensorModule simulator = new SensorModule();
-		HardwareModule hardware = new HardwareModule();
-
+	public void start(Stage primaryStage) {
 		// TODO: at startup ask for desired values, save them to 'desired' ArrayList
-
 		// TODO: determine at what time interval these will be changed
-		current.set(0, simulator.generateTemperature());
-		current.set(1, simulator.generateHumidity());
-		current.set(2, simulator.generateMoisture());
-		current.set(3, simulator.generatePH());
-		current.set(4, simulator.generateCO2());
+		
 
-		// These prints will eventually be migrated to the Display
-		System.out.println("The current temperature is " + current.get(0) + " degrees Fahrenheit");
-		System.out.println("The current humidity is " + current.get(1) + "%");
-		System.out.println("The current soil moisture is " + current.get(2) + "%");
-		System.out.println("The current soil pH is " + current.get(3));
-		System.out.println("The current CO2 level is " + current.get(4) + "ppm");
+		display.setLbl_temperature(current.get(0));
+		display.setLbl_humidity(current.get(1));
+		display.setLbl_moisture(current.get(2));
+		display.setLbl_PH(current.get(3));
+		display.setLbl_CO2(current.get(4));
+		
+		primaryStage.setTitle("IntelliGarden");
+		Scene mainScene = new Scene(display, 500, 500);
+		primaryStage.setScene(mainScene);
+		primaryStage.show();
 		
 		//hardware.checkAir(current.get(0), desired.get(0));
 		//display: update current temperature and A/C status
-
 	}
 
 }
--- Display.java
+++ Display.java
@@ -1,9 +1,67 @@
 package intelliGreen;
 
-public class Display {
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+public class Display extends BorderPane{
 	
 	/*
 	 * This class creates the user Display using JavaFX
 	 */
+	Label lbl_temperature = new Label();
+	Label lbl_humidity = new Label();
+	Label lbl_moisture = new Label();
+	Label lbl_PH = new Label();
+	Label lbl_CO2 = new Label();
+	VBox vbox_sensorValues = new VBox();
+	//TextField txtField_currentGuess = new TextField();
+	
+	public Display(){
+		setPadding(new Insets(10, 20, 20, 20));
+		vbox_sensorValues.getChildren().addAll(lbl_temperature, lbl_humidity, lbl_moisture, lbl_PH, lbl_CO2);
+		setTop(vbox_sensorValues);
+	}
+
+	public Label getLbl_temperature() {
+		return lbl_temperature;
+	}
+
+	public void setLbl_temperature(int temp) {
+		lbl_temperature.setText("Temperature: " + temp + " deg F");
+	}
+
+	public Label getLbl_humidity() {
+		return lbl_humidity;
+	}
+
+	public void setLbl_humidity(int humidity) {
+		lbl_humidity.setText("Humidity: " + humidity + "%");
+	}
+
+	public Label getLbl_moisture() {
+		return lbl_moisture;
+	}
+
+	public void setLbl_moisture(int moisture) {
+		lbl_moisture.setText("Moisture: " + moisture + "%");
+	}
+
+	public Label getLbl_PH() {
+		return lbl_PH;
+	}
+
+	public void setLbl_PH(int ph) {
+		lbl_PH.setText("PH: " + ph);
+	}
+
+	public Label getLbl_CO2() {
+		return lbl_CO2;
+	}
+
+	public void setLbl_CO2(int CO2) {
+		lbl_CO2.setText("CO2: " + CO2 + "ppm");
+	}
 
 }
